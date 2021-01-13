53bbc79d2a5ffc225ffd9164cbda35d31b862b7c
andrewdiclerico <diclerico.andrew@gmail.com>
2021-01-13 14:47:50 -0500

    Message

--- LabOneA.py
+++ LabOneA.py
@@ -7,7 +7,9 @@
 #-guidelines / information for anyone reading your documentation
 
 #VARIABLE DICTIONARY---------
-#
+#maxCap             maximum capacity of a room
+#numberOfPeople     Number of people attending room
+#overLimit          number of people over the limit
 
 #FUNCTIONS------------------------------------------------------------------
 
@@ -16,8 +18,18 @@
 #initialize variables
 maxCap = 0
 numberOfPeople = 0
+overLimit = 0
 
 #getting max room cap.
-input(print("What is the maximum capacity of the room?"))
+maxCap = float(input(print("What is the maximum capacity of the room?")))
 
 #getting number of people attending
+numberOfPeople = float(input(print("How many people are attending the room?")))
+
+if maxCap > numberOfPeople:
+    print("Meeting cannot be held as planned due to the fire regulations")
+
+    overLimit = numberOfPeople - maxCap
+
+    print("To carry on with the event, ", overLimit, "people must be excluded to meet fire regulations")
+    
