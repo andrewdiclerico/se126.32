50700c915b406dee72b6e2b7da0bd4de73756a3e
andrewdiclerico <65728667+andrewdiclerico@users.noreply.github.com>
2021-01-13 14:09:07 -0500

    first save

---
+++ LabOneA.py
@@ -0,0 +1,23 @@
+#LabOneA
+
+#STARTING DOCUMENTATION:
+#-program prompt
+#-pseudocode
+#-notes for you to remember
+#-guidelines / information for anyone reading your documentation
+
+#VARIABLE DICTIONARY---------
+#
+
+#FUNCTIONS------------------------------------------------------------------
+
+#BASE PROGRAM CODE----------------------------------------------------------
+
+#initialize variables
+maxCap = 0
+numberOfPeople = 0
+
+#getting max room cap.
+input(print("What is the maximum capacity of the room?"))
+
+#getting number of people attending
